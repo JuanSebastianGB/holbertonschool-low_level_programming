7962aa193ab7a528d15a5502ea1d537d8b035caf
JuanSebastianGB <3393@holbertonschool.com>
2021-06-18 03:39:27 +0000

    changing expected var like double

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-printf("Size of a char: %i byte(s)\n", (int) sizeof(char));
-printf("Size of a int: %i byte(s)\n", (int) sizeof(int));
-printf("Size of a long int: %i byte(s)\n", (int) sizeof(long int));
-printf("Size of a long long int: %i byte(s)\n", (int) sizeof(long long int));
-printf("Size of a float: %i byte(s)\n", (int) sizeof(float));
+printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
+printf("Size of a int: %d byte(s)\n", (int) sizeof(int));
+printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
 
 return (0);
 }
