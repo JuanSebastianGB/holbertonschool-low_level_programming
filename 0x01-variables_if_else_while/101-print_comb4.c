32f0ab8735543e23f30101ca9090c52675d1aa55
JuanSebastianGB <3393@holbertonschool.com>
2021-06-19 14:19:22 +0000

    fixing outputs for betty and moving y incremental at the end of the while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,36 +11,34 @@
 
 int main(void)
 {
-  int i;
-  int j;
-  int z = 49;
-  int k;
-  int y = 50;
+int i;
+int j;
+int z = 49;
+int k;
+int y = 50;
 
-  i=48;
-  while(i <= 55)
-    {
+i = 48;
+while (i <= 55)
+{ /*Start of while */
 
-      for (j = z ; j <= 56; j++)
-	{
-	  for (k = y; k <= 57; k++)
-	    {
-	      putchar(i);
-	      putchar(j);
-	      putchar(k);
-  	      if (i != 55  &&  z != 56  && y != 57)
-		{
-		  putchar(44);
-		  putchar(32);
-		}
-	    }
-	  y++;
-	}
-      
-      i++;
-      z++;
-      
-    }
-  putchar(10);
-  return (0);
+for (j = z ; j <= 56; j++)
+{ /*Start of j for*/
+for (k = y; k <= 57; k++)
+{ /*star of k for*/
+putchar(i);
+putchar(j);
+putchar(k);
+if (i != 55  &&  z != 56  && y != 57)
+{
+putchar(44);
+putchar(32);
+}
+} /*End of k for*/
+} /*End of j for*/
+i++;
+z++;
+y++;
+} /*End of while*/
+putchar(10);
+return (0);
 }
