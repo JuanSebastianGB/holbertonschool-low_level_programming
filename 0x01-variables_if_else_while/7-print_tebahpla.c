9b5b03877c72b07dd75e35d66a3bf33cfaa4798b
JuanSebastianGB <3393@holbertonschool.com>
2021-06-19 03:52:04 +0000

    7. Smile in the mirror. Write a program that prints the lowercase alphabet in reverse, followed by a new line.

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char aux = 'z';
+
+while (aux >= 'a')
+{
+putchar(aux);
+if (aux == 'a')
+{
+putchar('\n');
+}
+aux--;
+}
+return (0);
+}
