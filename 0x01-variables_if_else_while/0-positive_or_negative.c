db41c38cfbc3cc9a9cfb13ffbe9b1267b41a2954
JuanSebastianGB <3393@holbertonschool.com>
2021-06-19 01:53:17 +0000

    Making some fixes of syntaxis and showind the value in the print

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,27 +1,31 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
+/* betty style doc for function main goes there */
 /**
- * Main function : print a msj depending on the input n
- * More here
- * return 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-  int n;
+int n;
 
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  if (n > 0)
-    {
-      printf("is positive\n");
-    } else if (n == 0)
-    {
-      printf("is zero\n");
-    } else
-    {
-      printf("is negative\n");
-    }
-  return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 1;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n",n);
+}
+else
+{
+  printf("%d is negative\n",n);
+}
+return (0);
 }
