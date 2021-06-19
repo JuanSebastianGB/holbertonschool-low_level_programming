0cceec2f8ba66ca539f0ea435915e10ec0d38cf3
JuanSebastianGB <3393@holbertonschool.com>
2021-06-19 03:03:48 +0000

    3. alphABET Write a program that prints the alphabet in lowercase, and then in uppercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char i, j;
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+for (j = 'A'; j <= 'Z'; j++)
+{
+putchar(j);
+if (j == 'Z')
+{
+putchar('\n');
+}
+}
+return (0);
+}
