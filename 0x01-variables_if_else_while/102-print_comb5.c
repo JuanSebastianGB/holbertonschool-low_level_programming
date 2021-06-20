95c5b0cc71fc97d5195c7d1e9f72e4241c3bb96a
JuanSebastianGB <3393@holbertonschool.com>
2021-06-20 04:28:07 +0000

    Changing logic, moving the value of k and l at the begining of loops, and removing some unnecesary for

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,19 +8,18 @@
  */
 int main(void)
 {
-int i = 48;
-int j = 48;
-int k = i;
-int l = 49;
+int i = 48, j = i, k, l;
+
 while (i <= 57) /* First digit*/
 {
+j = 48;
 while (j <= 57) /* Second digit */
 {
+k = i;
+l = j + 1;
 while (k <= 57) /* Third digit*/
 {
 while (l <= 57) /* Fourth digit*/
-{
-if (!(i == k && j == l)) /* Deleting case when repeat pairs 1 and 2*/
 {
 putchar(i);
 putchar(j);
@@ -28,24 +27,19 @@ putchar(32);
 putchar(k);
 putchar(l);
 if (!(i == 57 && j == 56 && k == 57 && l == 57))
-{ putchar(44); }
-l++;
-}
-} /*End loop of fourth digit*/
-if (l == 58 && k == 57)
-{ l = j + 2; }
-else
 {
-l = 49; /* min value by default is 49 */
-} /*End if*/
+putchar(44);
+putchar(32);
+}
+l++;
+} /*End loop of fourth digit*/
+l = 48;
 k++;
 } /*End loop of third digit*/
-k = i; /* Min value of third digit is equal i*/
 j++;
 } /*End loop of second digit */
 i++;
-j = 48; /* Min value of second digit is 1 */
 } /*End loop of first digit*/
-putchar('\n');
+putchar(10);
 return (0);
 }
