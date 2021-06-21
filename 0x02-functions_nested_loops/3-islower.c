790b2516cfb5476647c596e9e696f1b7126b6daa
JuanSebastianGB <3393@holbertonschool.com>
2021-06-21 11:21:50 -0700

    making some betty´s corrections, specialy in the description

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * _islower - function to identify is lower case
+ * @c: input
+ *
+ * Return: 1 if lowercase and 0 anything else
+ */
+
+int _islower(int c)
+{
+int i, aux = 0;
+for (i = 97; i <= 122; i++)
+{
+if (i == c)
+{
+aux++;
+}
+}
+if (aux > 0)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
