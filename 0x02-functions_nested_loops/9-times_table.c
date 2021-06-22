cae115a9fd7262be1e628cda128bbf43d820e60a
JuanSebastianGB <3393@holbertonschool.com>
2021-06-22 06:09:53 +0000

    9. Learn your times table. Write a function that prints the 9 times table, starting with 0.

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * times_table -  Prints the 9 time table
+ *
+ *
+ * Return: Print
+ */
+
+void times_table(void)
+{
+int y, x, xy;
+for (x = 0; x <= 9; x++)
+{
+for (y = 0; y <= 9; y++)
+{
+xy = x * y; /* This is the pattern */
+if (xy < 10)
+{
+_putchar(xy + '0');
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+}
+else
+{
+_putchar(xy / 10 + '0'); /* result is int part (first digit) */
+_putchar(xy % 10 + '0'); /* result is second digit */
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+}
