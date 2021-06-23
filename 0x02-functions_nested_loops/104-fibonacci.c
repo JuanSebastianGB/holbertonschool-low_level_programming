61d2ad93532e45ac88bedd9ec7c3cc9dfa242973
JuanSebastianGB <3393@holbertonschool.com>
2021-06-23 03:58:35 +0000

    16. In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Write a program that finds and prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print fibonacci numbers
+ *
+ * Return: First 50 fibonacci numbers
+ */
+
+int main(void)
+{
+long int i = 0, j = 1, result, aux = 0;
+while (aux < 98)
+{
+result = i + j;
+printf("%li, ", result);
+i = j; /* i assum j's before value */
+j = result; /* j becomes the before result */
+aux++;
+}
+printf("%li\n", i + j);
+return (0);
+}
