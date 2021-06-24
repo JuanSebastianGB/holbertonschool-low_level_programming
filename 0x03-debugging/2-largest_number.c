4d2106206820922ab455c6cb3dd2ae4d2ae5085f
JuanSebastianGB <3393@holbertonschool.com>
2021-06-24 04:37:35 +0000

    Making betty corrections

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,19 @@
 
 int largest_number(int a, int b, int c)
 {
-  int largest;
+int largest;
 
-  if (a > b)
-    {
-      largest = a;
-    }else
-    {
-      largest = b;
-    }
-
-  if (c > largest)
-    {
-      largest = c;
-    }
-  
-  return (largest);
+if (a > b)
+{
+largest = a;
+}
+else
+{
+largest = b;
+}
+if (c > largest)
+{
+largest = c;
+}
+return (largest);
 }
