674dc7fd21a9bde9c8aff444e04c9836a8906e17
JuanSebastianGB <3393@holbertonschool.com>
2021-06-25 01:01:44 +0000

    uprading with betty solved

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,8 +3,8 @@
 
 /**
  * print_numbers - multiplication between 2 numbers
- * @a: Integer to be evaluated
- * @b: Integer to be evaluated
+ *
+ *
  * Return: print
  *
  */
@@ -13,7 +13,7 @@
 void print_numbers(void)
 {
 int i = '0';
-while(i <= '9')
+while (i <= '9')
 {
 _putchar(i);
 i++;
