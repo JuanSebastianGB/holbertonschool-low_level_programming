6b58d02181fec7003a09b7441f33098e62c8af10
JuanSebastianGB <3393@holbertonschool.com>
2021-06-26 03:28:35 +0000

    adding \n at the end

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -15,7 +15,7 @@ for (i = 1; i <= n; i++)
 if (n % i == 0) /* identfying numbers that are divisible */
 {
 if (n == i) /* The last iteration */
-printf("%li", i);
+printf("%li\n", i);
 n = n / i; /* n gets a new value after this iteration */
 i = 1; /* Reset i value for the lopp of the new number*/
 }
