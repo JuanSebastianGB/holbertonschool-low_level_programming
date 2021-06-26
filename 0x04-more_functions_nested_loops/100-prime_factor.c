d1ccf3766a6b590fb8258a1b293568e6bb0a74fb
JuanSebastianGB <3393@holbertonschool.com>
2021-06-26 03:26:18 +0000

    11. The problem of distinguishing prime numbers. Write a program that finds and prints the largest prime factor of the number 612852475143, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always(0);
+ */
+
+int main(void)
+{
+long int i, n = 612852475143;
+
+for (i = 1; i <= n; i++)
+{
+if (n % i == 0) /* identfying numbers that are divisible */
+{
+if (n == i) /* The last iteration */
+printf("%li", i);
+n = n / i; /* n gets a new value after this iteration */
+i = 1; /* Reset i value for the lopp of the new number*/
+}
+}
+return (0);
+}
