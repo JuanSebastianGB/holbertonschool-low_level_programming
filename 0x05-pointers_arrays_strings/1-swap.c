241ffa16251523aa607c381898717db4dc44317b
JuanSebastianGB <3393@holbertonschool.com>
2021-06-28 06:32:25 -0700

    uploading file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *swap_int - change values
+ *@a: input pointer
+ *@b: input pointer
+ *Return: Not return
+ */
+
+void swap_int(int *a, int *b)
+{
+  int c;
+  c = *a;
+  *a = *b;
+  *b = c;  
+
+}
