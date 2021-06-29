f77ba6687441cfc78fea1abb1828a2191443b5f1
JuanSebastianGB <3393@holbertonschool.com>
2021-06-29 01:56:30 +0000

    adding 6-puts2.c file of pointt 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ *puts2 - prints every other character of a string
+ *@str: string to be used
+ *
+ *Return: print
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (*(str + i) % 2 == 0)
+			_putchar(*(str + i));
+		i++;
+	}
+
+}
