677c92d62b26179b9f87d8668ae13095ef9e7ced
JuanSebastianGB <3393@holbertonschool.com>
2021-07-02 05:19:58 +0000

    uploading file that allow us to comparte 2 strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * *_strcmp - compare two strings
+ *@s1: pointer 1
+ *@s2: pointer 2
+ *Return: A pointer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, aux = 0, aux2 = 0, result = 0;
+
+	while (*(s1 + i))
+	{
+		aux = aux + s1[i];
+		i++;
+	}
+	i = 0;
+	while (*(s2 + i))
+	{
+		aux2 = aux2 + s2[i];
+		i++;
+	}
+	if (aux > aux2)
+		result = 15;
+	if (aux < aux2)
+		result = -15;
+
+	return (result);
+}
