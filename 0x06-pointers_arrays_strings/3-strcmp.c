c9ad2e30527ca6f4ddbd91793f2bb4a1f282ed84
JuanSebastianGB <3393@holbertonschool.com>
2021-07-02 10:32:52 -0700

    change the logic, identify when get '\0' in both strings and are the same

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,23 +9,10 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, aux = 0, aux2 = 0, result = 0;
+	int i = 0;
+	
+	while (s1[i] != '\0' && s2[i] != '\0' && (s1[i] == s2[i]))
+	  i++;
 
-	while (*(s1 + i))
-	{
-		aux = aux + s1[i];
-		i++;
-	}
-	i = 0;
-	while (*(s2 + i))
-	{
-		aux2 = aux2 + s2[i];
-		i++;
-	}
-	if (aux > aux2)
-		result = 15;
-	if (aux < aux2)
-		result = -15;
-
-	return (result);
+	return (s1[i] - s2[i]);
 }
