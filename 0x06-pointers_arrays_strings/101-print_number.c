8590f05bb39e3f0771566fae4076698a0b13b647
JuanSebastianGB <3393@holbertonschool.com>
2021-07-03 04:36:35 +0000

    uploading script that print an integer using _putchar and recursive function

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "holberton.h"
+
+
+/**
+ * print_number - Function that prints an integer number
+ *@n: int to print
+ *
+ *Return: print
+ */
+
+void print_number(int n)
+{
+	int bu = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		bu = bu * -1;
+	}
+
+	if ((bu / 10) > 0)
+		print_number(bu / 10);
+	_putchar((bu % 10) + '0');
+
+
+}
