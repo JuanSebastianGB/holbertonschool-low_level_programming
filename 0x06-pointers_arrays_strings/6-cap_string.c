c3c2a3048f4a00e7bdfe562df41f4ea76a2f04e4
JuanSebastianGB <3393@holbertonschool.com>
2021-07-03 02:41:34 +0000

    changing the aux array structure and adding case, first letter has to be upper

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,28 +11,26 @@
 
 char *cap_string(char *n)
 {
-	int i = 0, j, aux2 = 0, aux3 = 0;
-	int aux[] = {10, 32, 9, 44, 59, 46, 33, 63, 34, 41, 42, 123, 125 };
-	int size = sizeof(aux) / sizeof(int);
+	int i = 0, j;
+	char aux[] = " \t\n,;.!?\"(){}";
+	int size = sizeof(aux) / sizeof(char);
 
-	while (*(n + i))
+	while (n[i])
 	{
-		if (i > 0)
+		if (n[i] >= 'a' && n[i] <= 'z')
 		{
-			for (j = 0; j < size; j++)
+			if (i == 0)
+				n[i] = n[i] - 32;
+			else
 			{
-				if (*(n + i - 1) == aux[j])
-					aux2++;
-				if (*(n + i) != aux[j])
-					aux3++;
+				for (j = 0; j < size; j++)
+				{
+					if (n[i - 1] == aux[j])
+						n[i] = n[i] - 32;
+				}
 			}
-			if (*(n + i) >= 'a' && *(n + i) <= 'z' && aux2 > 0 && aux3 > 0)
-				*(n + i) = *(n + i) - 32;
-			aux2 = 0;
-			aux3 = 0;
 		}
 		i++;
 	}
-
 	return (n);
 }
