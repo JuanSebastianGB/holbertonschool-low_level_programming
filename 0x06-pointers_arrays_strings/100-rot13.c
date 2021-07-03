3d80465d26d1797e926bb76aa0acf30cb0dc0202
JuanSebastianGB <3393@holbertonschool.com>
2021-07-03 03:38:38 +0000

    uploading script that codify rot13, task 8, testing with checker

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "holberton.h"
+
+
+/**
+ * *rot13 - Function that codify a char
+ *@n: Char to change
+ *
+ *Return: char changed
+ */
+
+char *rot13(char *n)
+{
+	char aux1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char aux2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int i = 0, j, size = (int) sizeof(aux1) / sizeof(char);
+
+	while (*(n + i))
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (*(n + i) == aux1[j])
+			{
+				*(n + i) = aux2[j];
+				break; /* Out of the for at he moment that are same */
+			}
+		}
+		i++;
+	}
+	printf("\n %i", size);
+	return (n);
+}
