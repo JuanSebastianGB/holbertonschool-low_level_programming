59019519136c86f615b8f6c043b8b349b1bc579e
JuanSebastianGB <3393@holbertonschool.com>
2021-07-03 03:04:44 +0000

    uploading script that codify a char, task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "holberton.h"
+
+
+/**
+ * *leet - Function that codify a char
+ *@n: Char to change
+ *
+ *Return: char changed
+ */
+
+char *leet(char *n)
+{
+	int i = 0, j;
+	char aux1[] = "aeotlAEOTL";
+	char aux2[] = "4307143071";
+
+	while (*(n +i))
+	{
+		for (j = 0; j < (int)(sizeof(aux1) / sizeof(char)); j++)
+		{
+			if (*(n + i) == aux1[j])
+				*(n + i) = aux2[j];
+		}
+
+		i++;
+	}
+	return (n);
+}
