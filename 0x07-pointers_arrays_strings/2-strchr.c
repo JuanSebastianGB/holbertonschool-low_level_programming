cb6b7453c4b6a679fd4bb08c3b419c6ec9e29e55
JuanSebastianGB <3393@holbertonschool.com>
2021-07-05 17:36:50 +0000

    uploding function that Returns a pointer to the first occurrence of the character c in the string s, or NULL if the character is not found, task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * *_strchr - function that locates a character in a string.
+ *@s: string in which one we are going to search
+ *@c: char to be shearch
+ *
+ *Return: char pointer to the first ocurrence of c
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0, find = 0;
+
+	while (*(s + i))
+	{
+		if (*(s + i) == c)
+		{
+			find = 1;
+			break;
+		}
+		i++;
+	}
+	if (find > 0)
+		return (s + i);
+	else
+		return (NULL);
+}
