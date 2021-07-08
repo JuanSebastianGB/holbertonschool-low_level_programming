4629b600704275c74341b52b4ed813d1e2e8cff2
JuanSebastianGB <3393@holbertonschool.com>
2021-07-08 06:49:22 +0000

    uploading blank file 101-wildcmp.c, for template, task 8

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * wildcmp - 1 if the strings can be considered identical, otherwise return 0.
+ *@s1: String 1
+ *@s2: String 2 with special char *
+ *Return: integer 1 if are identical
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	return (0);
+}
