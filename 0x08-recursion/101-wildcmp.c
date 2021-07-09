36e52a51bb3b6f0e00e55da403417a8ae35f7a1a
JuanSebastianGB <3393@holbertonschool.com>
2021-07-09 04:47:08 +0000

    uploading file 101-wildcmp.c, task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "holberton.h"
+
 /**
  * wildcmp - 1 if the strings can be considered identical, otherwise return 0.
  *@s1: String 1
@@ -9,5 +10,20 @@
 
 int wildcmp(char *s1, char *s2)
 {
+
+	if (!*s1 && !*s2) /*Min expresion*/
+		return (1);
+	if (!*s2 || (!*s1 && *s2 != '*')) /*not equal for sure*/
+		return (0);
+	if (*s2 == *s1) /* Normal case, both equal increment */
+		return (wildcmp(s1 + 1, s2 + 1));
+	/*Case ma********************************c*/
+	if (!*s1 && *s2 == '*')
+		return (wildcmp(s1, s2 + 1));
+	/* s1 can have to '*' which is not wildcard */
+	if (*s1 && *s2 == '*')
+		return (wildcmp(s1 + 1, s2) | wildcmp(s1, s2 + 1));
 	return (0);
+
+
 }
