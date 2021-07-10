0f83bf8d5ee43d65ff12a01bcf9689a132f1a2bc
JuanSebastianGB <3393@holbertonschool.com>
2021-07-10 00:59:43 +0000

    Adding main function description and using a case of couynter = 0 or less

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+/**
+ *main - entry point
+ *@argc: number of arguments called in compilation.
+ *@argv: Array that include the elements used to compile
+ *Return: always 0
+ **/
+
 int main(int argc, char *argv[])
 {
+	if (argc < 1)
+		return (-1);
+
 	printf("%s\n", *(argv + argc - 1));
 
 	return (0);
