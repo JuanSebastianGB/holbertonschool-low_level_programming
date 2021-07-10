9b1260acf78ee47328a7e6fdad8aad719e955fca
JuanSebastianGB <3393@holbertonschool.com>
2021-07-10 03:11:57 +0000

    uploading 100-change.c file template

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ *main - entry point
+ *@argc: number of arguments called in compilation.
+ *@argv: array that has the elements used to compile
+ *Return: always 0
+ **/
+
+int main(int argc, char *argv[])
+{
+
+	return (0);
+}
