acef46c5212d1b51056c974550faae36681073ec
JuanSebastianGB <3393@holbertonschool.com>
2021-07-14 01:37:51 +0000

    adding condition str == null

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 #include <stdlib.h>
-
+#include <stdio.h>
 
 /**
  * *_strdup - returns a pointer to a new string copyed from orig.
@@ -17,7 +17,8 @@ char *_strdup(char *str)
 	unsigned int i;
 
 	copy = malloc(sizeof(str));
-	if (copy == NULL)
+	
+	if (copy == NULL || str == NULL)
 		return (NULL);
 	for (i = 0; *(str + i); i++)
 		*(copy + i) = *(str + i);
