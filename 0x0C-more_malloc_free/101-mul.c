c0e0b86061515682314653acb28d737b946a341e
JuanSebastianGB <3393@holbertonschool.com>
2021-07-17 04:53:33 +0000

    uploading template 101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - not set yet
+ * @arg: length of argv
+ * @argv: input array
+ * Return: 0
+ */
+
+int main(int arg, char *argv[])
+{
+	char *s;
+
+	s = argv[2][2];
+
+	return (arg);
+}
