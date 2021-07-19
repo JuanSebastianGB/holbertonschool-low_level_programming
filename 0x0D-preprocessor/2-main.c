153283f3a12ac5519c24e0cc7abffc77a3da66ce
JuanSebastianGB <3393@holbertonschool.com>
2021-07-19 21:19:28 +0000

    adding file 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the file was compiled for
+ *
+ *Return: print
+ *
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
