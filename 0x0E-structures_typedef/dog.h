1676955b075fb749cd887d8a1a470fa461922836
JuanSebastianGB <3393@holbertonschool.com>
2021-07-20 00:38:13 +0000

    defining new type of dog like dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,9 @@ struct dog
 	char *owner;
 };
 
+
+typedef struct dog dog_t;/* defining new type of structure dog */
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
