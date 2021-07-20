1c8877b35780cd1092a80e1b249488de2448c4de
JuanSebastianGB <3393@holbertonschool.com>
2021-07-20 00:29:09 +0000

    adding '\n' in the null owner case

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,7 +18,7 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		printf("Age: %.6f\n", d->age);
 		if ((*d).owner == NULL)
-			printf("Owner: (nil)");
+			printf("Owner: (nil)\n");
 		else
 			printf("Owner: %s\n", (*d).owner);
 	}
