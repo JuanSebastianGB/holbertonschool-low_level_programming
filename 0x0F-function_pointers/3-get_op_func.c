dfd6c81d8b9cdf87b235656e6bfa57f67aa10df4
JuanSebastianGB <3393@holbertonschool.com>
2021-07-22 05:46:21 +0000

    changing logic, using strcmp

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -23,9 +23,9 @@ int (*get_op_func(char *s))(int, int)
 
 	while (ops[i].op != NULL)
 	{
-		if (*(ops[i].op) == *s)
-			return (ops[i].f);
+		if (!strcmp(ops[i].op, s))
+			break;
 		i++;
 	}
-	return (NULL);
+	return (ops[i].f);
 }
