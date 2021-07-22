8ec9cea73f579b41963aa707b37f5769b61cd620
JuanSebastianGB <3393@holbertonschool.com>
2021-07-22 04:56:52 +0000

    uploading required files, last task

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include "3-calc.h"
+
+/**
+ * main - Main function, the entry point
+ * @argc: number of input elements
+ * @argv: array with the imput elements
+ * Return: void
+ */
+
+int main(int argc, char *argv[])
+{
+
+	int number1, number2, output;
+	int (*function)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	number1 = atoi(argv[1]);
+	number2 = atoi(argv[3]);
+
+	function = get_op_func(argv[2]);
+
+	if (function == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	output = function(number1, number2);
+
+	printf("%d\n", output);
+
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,69 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds a and b
+ * @a: first number
+ * @b: second number
+ *
+ * Return: sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substract a and b
+ * @a: first number
+ * @b: second number
+ *
+ * Return: substraction of a and b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies a and b
+ * @a: first number
+ * @b: second number
+ *
+ * Return: multiplication of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divide a and b
+ * @a: first number
+ * @b: second number
+ *
+ * Return: divition of a and b
+ */
+int op_div(int a, int b)
+{
+	if (!b)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+/**
+ * op_mod - gets the mod of a with b
+ * @a: first number
+ * @b: second number
+ *
+ * Return: mod of a and b
+ */
+int op_mod(int a, int b)
+{
+	if (!b)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
