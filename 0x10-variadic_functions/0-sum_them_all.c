2f16fd033050e043dc8532f0daace5726e479e80
JuanSebastianGB <3393@holbertonschool.com>
2021-07-24 01:20:05 +0000

    making some test

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
 
 /**
  * sum_them_all - apply the sum operation to n imputs
  * @n: number of inputs
  * Return: sum of all input integers
- *
- *
  */
 
 
@@ -14,7 +13,7 @@ int sum_them_all(const unsigned int n, ...)
 {
 
 	va_list list;
-	int sum = 0;
+	unsigned int sum = 0;
 	unsigned int i = 0;
 
 	va_start(list, n);
