1c57d4dbe039547f1ea9118499b092a508de4654
JuanSebastianGB <3393@holbertonschool.com>
2021-07-31 01:47:28 +0000

    UPD: Addign prototype size_t list_len(const list_t *h);

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
+#include <stddef.h>
 
 /**
  * struct list_s - singly linked list
@@ -22,5 +23,6 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif /* _LISTS_H */
