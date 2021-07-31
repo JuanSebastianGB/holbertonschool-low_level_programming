8bcdbc4adc05ca972802e13a69b2fea621b30136
JuanSebastianGB <3393@holbertonschool.com>
2021-07-31 04:13:25 +0000

    UPL: uploading file 100-first.c, Advaced task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * i_first - function that execute before main
+ * Return: Nothing - in this case just printing
+ */
+void i_first(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my ");
+	printf("back!\n");
+}
