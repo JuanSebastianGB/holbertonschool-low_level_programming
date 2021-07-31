861aa6dd7dfce750e7220ae47ef104fd7e9fa4e5
JuanSebastianGB <3393@holbertonschool.com>
2021-07-31 02:30:52 +0000

    UPL: Adding file 2-add_node.c, task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * *add_node - Adds a new node at the begginign of list_t list
+ * @head: pointer to a pointed to a list
+ * @str: char to add to the node
+ *
+ * Return: a pointer to the new node
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *node_to_add;
+	int i = 0;
+
+	if (!str)
+		return (NULL);
+
+	node_to_add = malloc(sizeof(list_t));
+	if (!node_to_add)
+		return (NULL);
+
+	node_to_add->str = strdup(str);
+	if (!node_to_add)
+	{
+		free(node_to_add);
+		return (NULL);
+	}
+	while (*(node_to_add->str + i))
+		i++;
+	node_to_add->len = i;
+	node_to_add->next = *head; /* call itself */
+	*head = node_to_add;
+	return (node_to_add);
+}
