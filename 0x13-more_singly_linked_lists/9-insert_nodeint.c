c2f9ac4e7054c23f5f21f760f5b7b3c882989a83
JuanSebastianGB <3393@holbertonschool.com>
2021-08-03 05:42:21 +0000

    TEST: Changing *head by copy

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -41,7 +41,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx  == 0) /* case to at the beggining */
 	{
-		new_node->next = *head;
+		new_node->next = copy;
 		*head = new_node;
 		return (new_node);
 	}
