2d2528dadbc87434c05d4cf08e9654448d5fb016
JuanSebastianGB <3393@holbertonschool.com>
2021-08-03 01:44:42 +0000

    STYLE: Adding descitption to 4-free_listint.c function

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * free_listint - free memory allocated with malloc
+ * @head: pointer of kind listint_t, is the head of list
+ * Return - Nothing
+ */
+
+
 void free_listint(listint_t *head)
 {
 	listint_t *copy;
