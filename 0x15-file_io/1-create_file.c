e15536d508a459ef63b161307c6e4c777aefecf7
JuanSebastianGB <3393@holbertonschool.com>
2021-08-10 03:48:40 +0000

    FIX: Changing text_content not null to incriment the lenght

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	if (!text_content)
+	if (text_content)
 		len = _strlen(text_content);
 
 	/* truncate and assing read and write permission 6= 110 */
