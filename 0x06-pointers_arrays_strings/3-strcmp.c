f4abc6d5a1317e429cdbe9a062a92aeff1b549bf
JuanSebastianGB <3393@holbertonschool.com>
2021-08-15 01:16:00 +0000

    STYLE: Changing tab showing for better understanding

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,13 +9,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-int i = 0;
+	int i = 0;
 
-while (*(s1 + i) && *(s2 + i))
-{
-if (s1[i] != s2[i])
-break;
-i++;
-}
-return (s1[i] - s2[i]);
+	while (*(s1 + i) && *(s2 + i))
+	{
+		if (s1[i] != s2[i])
+			break;
+		i++;
+	}
+	return (s1[i] - s2[i]);
 }
