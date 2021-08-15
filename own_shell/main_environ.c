ffb8028b2e12d82a9c3a629034eea6f28951305d
JuanSebastianGB <3393@holbertonschool.com>
2021-08-15 14:41:14 +0000

    UPL: Uploading file main_environ.c, which prints the enviroment passed by argument in main function and passed like extern (global) variable

---
+++ own_shell/main_environ.c
@@ -0,0 +1,34 @@
+#include "shell.h"
+
+/*This one must be avoided*/
+extern char **environ;
+
+/**
+ * main - Print in both ways, like param
+ * and like global variable the enviroment
+ * @argc: Number of args that receive the program
+ * @av: char array with params passed to program
+ * @env: enviroment variable
+ * Return: Always 0 - printing
+ *
+ */
+int main(int argc, char **av __attribute__((unused)), char **env)
+{
+	int i = 0;
+
+	UNUSED(argc);
+	printf("main environ variable printing\n");
+	while (env[i])
+	{
+		printf("%s\n", env[i]);
+		i++;
+	}
+	i = 0;
+	printf("\n Here start environ global var printing\n");
+	while (environ[i])
+	{
+		printf("%s\n", environ[i]);
+		i++;
+	}
+	return (0);
+}
