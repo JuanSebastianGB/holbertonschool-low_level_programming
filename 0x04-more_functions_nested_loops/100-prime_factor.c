5eec10528595a9cbc81d7a94b1ccdaef9ca63023
JuanSebastianGB <3393@holbertonschool.com>
2021-08-17 14:12:29 +0000

    STYLE: Making more readable

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,17 +8,17 @@
 
 int main(void)
 {
-long int i, n = 612852475143;
+	long int i, n = 612852475143;
 
-for (i = 1; i <= n; i++)
-{
-if (n % i == 0) /* identfying numbers that are divisible */
-{
-if (n == i) /* The last iteration */
-printf("%li\n", i);
-n = n / i; /* n gets a new value after this iteration */
-i = 1; /* Reset i value for the lopp of the new number*/
-}
-}
-return (0);
+	for (i = 1; i <= n; i++)
+	{
+		if (n % i == 0) /* identfying numbers that are divisible */
+		{
+			if (n == i) /* The last iteration */
+				printf("%li\n", i);
+			n = n / i; /* n gets a new value after this iteration */
+			i = 1; /* Reset i value for the lopp of the new number*/
+		}
+	}
+	return (0);
 }
