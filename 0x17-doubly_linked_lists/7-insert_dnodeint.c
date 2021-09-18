5d30b3f1b126c289d4ff54078646afd135ff4d59
JuanSebastianGB <3393@holbertonschool.com>
2021-09-18 01:31:46 +0000

    UPL: Uploading files to implment a function that add a node in a gived index

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * *insert_dnodeint_at_index - inserts a new node at a given position.
+ * @h: Header node
+ * @idx: Index where to insert
+ * @n: integer value of the added node
+ * Return: Address of the indexed node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *copy = *h, *new = NULL;
+	size_t i = 0;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL || *h == NULL)
+		return (NULL);
+	new->n = n;
+	while (copy != NULL)
+	{
+		if (idx == 0)
+		{
+			new->next = copy;
+			new->prev = NULL;
+			copy->prev = new;
+			*h = new;
+			return (new);
+		}
+		if (idx == i)
+		{
+			new->next = copy;
+			new->prev = copy->prev;
+			copy->prev->next = new;
+			copy->prev = new;
+			return (new);
+		}
+		copy = copy->next;
+		i++;
+	}
+	return (NULL);
+}
