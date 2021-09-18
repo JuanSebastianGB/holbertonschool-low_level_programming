28d8010b8f74c883aa4fd9eb5519a7c1a0b2a995
JuanSebastianGB <3393@holbertonschool.com>
2021-09-18 02:28:06 +0000

    FIX: Changing *h by h

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -33,7 +33,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	size_t i = 0;
 
 	new = malloc(sizeof(dlistint_t));
-	if (new == NULL || *h == NULL)
+	if (new == NULL || h == NULL)
 		return (NULL);
 	new->n = n;
 	while (copy != NULL)
