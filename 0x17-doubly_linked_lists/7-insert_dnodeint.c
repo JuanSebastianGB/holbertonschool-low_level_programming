955733347822af8b85699deb602fe997bd955972
JuanSebastianGB <3393@holbertonschool.com>
2021-09-18 02:29:58 +0000

    UPD: Deleting unused function

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,26 +1,6 @@
 #include "lists.h"
 
 /**
- * *get_dnodeint_at_index - returns the nth node of a dlistint_t linked list.
- * @head: Head node of the list
- * @index: Index to return
- * Return: Node indexed
- */
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
-{
-	size_t i = 0;
-
-	while (head != NULL)
-	{
-		if (i == index)
-			return (head);
-		head = head->next;
-		i++;
-	}
-	return (NULL);
-}
-
-/**
  * *insert_dnodeint_at_index - inserts a new node at a given position.
  * @h: Header node
  * @idx: Index where to insert
